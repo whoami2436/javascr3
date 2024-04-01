a04edd34217a81c28d4667d5b7c4f70718bec551
Whoami <nurlanguliev33@gmail.com>
2024-04-01 08:48:40 -0700

    javascr3

---
+++ app.js
@@ -0,0 +1,106 @@
+// TASK-1
+// let obyekt = {
+//   id: 1,
+//   name: "Araz",
+//   surname: "Khaliloph",
+//   hobby: "reading"
+// }
+// console.log(obyekt);
+
+
+// TASK-2
+// let array = [
+//   {
+//     id: 1,
+//     fin: "AA24MT7",
+//     age: 27,
+    // height: "1.9 m"
+//   },
+//   {
+//     id: 2,
+//     fin: "AA256TB",
+//     age: 22,
+//     height: "1.6 m"
+//   }
+// ];
+// console.log(array);
+
+
+
+// TASK-3
+// let task = [
+//     {
+//       id: 2,
+//       name: "Nurlan",
+//       surname: "Guliyev",
+//       age: 20,
+//       game_id: 22,
+//     },
+//     {
+//       id: 3,
+//       name: "Turan",
+//       surname: "Meherremov",
+//       age: 20,
+//       game_id: 22,
+//     },
+//     {
+//       id: 4,
+//       name: "Cavid",
+//       surname: "Ibrahimov",
+//       age: 20,
+//       game_id: 33,
+//     },
+//   ];
+//   let game = [
+//     {
+//       id: 22,
+//       sort: "Football",
+//     },
+//     {
+//       id: 33,
+//       sort: "Basetball",
+//     },
+//   ];
+//   task.map((task1) => {
+//     let games = game.find((Alt) => Alt.id === task1.game_id);
+//     console.log(`${task1.name} ${task1.surname} >> ${games.sort}`);
+//   });
+
+
+
+// TASK-4
+// function findAverage (array) {
+// let sum = 0;
+// for (let i = 0; i < array.lenght; i++){
+//   sum += array[i];
+// }
+// return sum / array.lenght;
+// }
+// const array = [1, 2, 3, 4, 5];
+// findAverage (array);
+
+
+// TASK-5
+let task = [
+    {
+        ad: "Nurlan",
+        soyad: "Guliyev",
+        bal: 89,
+    },
+    {
+        ad: "Khayyam",
+        soyad: "Musazade",
+        bal: 95,
+    },
+    {
+        ad: "Araz",
+        soyad: "Khalilli",
+        bal: 84,
+    },
+    {
+        ad: "Asger",
+        soyad: "Naghdaliyev",
+        bal: 92,
+    },
+]
+
